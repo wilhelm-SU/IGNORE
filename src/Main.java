e9e73d58f293c8dc4de56b8f85d79ea8c37675d1
Jadon Wilhelm <wilhelm8@go.stockton.edu>
2024-09-23 20:00:16 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello repository");
+        System.out.println("I am using this as a testing ground in order to...");
+        System.out.println("Learn how to use github effectively");
+
+    }
+}
